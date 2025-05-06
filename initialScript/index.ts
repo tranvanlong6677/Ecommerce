4c896472d83ba36647e2d4e2c276eb357db48263
Long Tran <tranvanlong6677@gmail.com>
2025-05-06 16:22:48 +0700

    initial seed data

---
+++ initialScript/index.ts
@@ -0,0 +1,61 @@
+import envConfig from 'src/shared/config'
+import { HashingService } from 'src/shared/services/hashing.service'
+import { PrismaService } from 'src/shared/services/prisma.service'
+import { RoleName } from 'src/shared/constants/role.constant'
+const prisma = new PrismaService()
+const hashingService = new HashingService()
+const main = async () => {
+  const roleCount = await prisma.role.count()
+  //   Nếu tạo role rồi thì throw error
+  if (roleCount > 0) {
+    throw new Error('Roles already exist')
+  }
+
+  //   Nếu chưa tạo role thì tạo
+  const roles = await prisma.role.createMany({
+    data: [
+      {
+        name: RoleName.Admin,
+        description: 'Admin role',
+      },
+      {
+        name: RoleName.Client,
+        description: 'Client role',
+      },
+      {
+        name: RoleName.Seller,
+        description: 'Seller role',
+      },
+    ],
+  })
+
+  //   Lấy ra admin role vừa tạo
+  const adminRole = await prisma.role.findFirstOrThrow({
+    where: {
+      name: RoleName.Admin,
+    },
+  })
+
+  //   Tạo ra user admin
+  const hashedPassword = await hashingService.hashPassword(envConfig.ADMIN_PASSWORD)
+  const adminUser = await prisma.user.create({
+    data: {
+      email: envConfig.ADMIN_EMAIL,
+      password: hashedPassword,
+      name: envConfig.ADMIN_NAME,
+      phoneNumber: envConfig.ADMIN_PHONE_NUMBER,
+      roleId: adminRole.id,
+    },
+  })
+  return {
+    createdRoleCount: roles.count,
+    adminUser,
+  }
+}
+
+main()
+  .then(({ adminUser, createdRoleCount }) => {
+    console.log(`Created ${createdRoleCount} roles`)
+    console.log(`Created admin user: ${adminUser.email}`)
+  })
+  .catch(console.error)
--- src/shared/config.ts
+++ src/shared/config.ts
@@ -20,6 +20,10 @@ export const configSchema = z.object({
   REFRESH_TOKEN_SECRET: z.string(),
   REFRESH_TOKEN_EXPIRES_IN: z.string(),
   SECRET_API_KEY: z.string(),
+  ADMIN_NAME: z.string(),
+  ADMIN_PASSWORD: z.string(),
+  ADMIN_EMAIL: z.string(),
+  ADMIN_PHONE_NUMBER: z.string(),
 })
 
 const configServer = configSchema.safeParse(process.env)
