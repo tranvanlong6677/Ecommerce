15e5815e6dee51bdbce376dc38b37866ca9cb077
Long Tran <tranvanlong6677@gmail.com>
2025-06-13 13:53:15 +0700

    Học về migration

---
+++ prisma/migrations/20250613035712_rename/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Permission" ALTER COLUMN "content" DROP DEFAULT;
--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,11 @@ import { ZodSerializerInterceptor } from 'nestjs-zod'
 import { HttpExceptionFilter } from './shared/filters/http.exception.filter'
 import CustomZodValidationPipe from './shared/pipes/custom-zod-validation.pipe'
 import { LanguageModule } from './routes/language/language.module'
+import { PermissionsModule } from './permissions/permissions.module';
+import { PermissionsModule } from './src/routes/permissions/permissions.module';
 
 @Module({
-  imports: [SharedModule, AuthModule, LanguageModule],
+  imports: [SharedModule, AuthModule, LanguageModule, PermissionsModule],
   controllers: [AppController],
   providers: [
     AppService,
